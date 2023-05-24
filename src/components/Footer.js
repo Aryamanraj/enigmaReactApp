dc0395cd592a3f6902d3b88c40937337a9619a0c
Aryamanraj <20cs3011@rgipt.ac.in>
2023-05-24 17:23:23 +0530

    adjusted

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import tornado from "../assets/footer-img.png"
+import logo from '../assets/ENIGMA-LOGO.png'
+import '../footer-page.css'
+
+export default function Footer(){
+    return(
+        <footer>
+            {/* <img className='footer-img' src={tornado} alt="tornado object" /> */}
+            {/* <div className="footer-cta">
+                
+                {/* </div> */}
+                
+            {/* </div> */} 
+            
+            {/* <hr /> */}
+        </footer>
+    )
+}
---
+++ src/components/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import SvgGrid from './svgGrid'
+import Nav from './Nav.js'
+import SvgSphere from './svgSphere'
+import SvgStar from './svgStar'
+import SvgTriangle from './SvgTriangle'
+import SvgSlingy from './svgSlingy'
+import Register from './Register'
+import '../footer-page.css'
+import { useNavigate} from 'react-router-dom'
+
+export default function HomePage(){
+    let navigate = useNavigate();
+    return (
+        <section className="Home">
+            <div className='home-div'>
+        <SvgGrid />
+        <SvgStar />
+        <SvgTriangle />
+        <SvgSlingy />
+        <SvgSphere />
+        <Nav />
+        <div className='home-txt'>
+                <h1>The easiest way to store data on the decentralised web.</h1>
+                <p>Store your data using our simple API. It’s fast, open, and it’s free.</p>
+            <button onClick={()=> (navigate("/login"))} className='demo-btn'><span>START STORING</span></button>
+        </div>    
+        </div>
+        {/* <Footer/> */}
+        </section>
+    )
+} 
